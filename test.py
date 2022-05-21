4250390bfb7ada397828016c958773890f779347
muna7657 <79190448+muna7657@users.noreply.github.com>
2022-05-21 15:16:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
-from flask import Flask, render_template
 import os
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+from flask import Flask, render_template, abort, url_for, json, jsonify
+import json
+app = Flask(__name__,template_folder='.')
+# read file
+with open('file.json', 'r') as myfile:
+    data = myfile.read()
+@app.route("/")
+def index():
+    return render_template('index.html', title="page", jsonfile=json.dumps(data))
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
